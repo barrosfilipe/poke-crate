c3df6130166c64de383bf80607f8169fb0b777b3
Filipe Barros <filipebarrossi@gmail.com>
2019-07-03 01:48:01 -0300

    first release

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center"><img src="https://i.imgur.com/XBnGCcH.png"></p>
+<h1 align="center">A Pokémon Loot Crate Style Game</h1>
+
+<p align="center">You click over the <b>Poké Ball</b> and a random <b>Pokémon</b> appears as a <u>loot</u>.</p>
+
+# How to use?
+
+Just `download` or `clone` the repository and open the `index.html` file.
+
+# Why?
+
+This quick project was created to motivate beginner developers to create their own very first applications
+for a `Twitch.tv` live stream challenge created by [@mbernardes19](https://www.twitch.tv/mbernardes19)
+
+# What does it cover?
+
+Basics of `HTML`, `CSS`, `JavaScript`, `Sound effects` and CSS animations with [Animate CSS](https://daneden.github.io/animate.css)
+
+# Screenshot
+
+<p align="center"><img src="https://i.imgur.com/axbKj5r.png"></p>
+
+# Working demonstration
+
+Visit [https://pokecrate.surge.sh](https://pokecrate.surge.sh) to see the project running.
+
+# Important
+
+_This project is not affiliated to the `Pokémon` or `Nintendo`_
+
+_Some data was collected from [The Spriters Resource](https://www.spriters-resource.com/nintendo_switch/pokemonquest/sheet/106163/)_
---
+++ js/index.js
@@ -0,0 +1,59 @@
+/* Add html elements to JS variables */
+var pokeball = document.querySelector(".pokeball");
+var pokemonFound = document.querySelector(".pokemon-found");
+var closePokemonFound = document.querySelector(".close");
+var pokemonImg = document.querySelector(".pokemon");
+var pokemonName = document.querySelector(".name");
+
+/* Pokeball audio */
+var pokeballAudio = new Audio("sfx/pokeball.mp3");
+pokeballAudio.volume = 0.1;
+
+pokeball.addEventListener("click", function() {
+  pokeballAudio.play();
+  pokeball.classList.add("animated", "shake");
+
+  /* Vibrate when using mobile */
+  window.navigator.vibrate([50, 50, 50]);
+
+  /* Generate random number between 1 and 151 */
+  var randomPokemon = Math.floor(Math.random() * 151) + 1;
+
+  /* Change Image SRC to the random generated number */
+  pokemonImg.src = `img/pokemons/${randomPokemon}.png`;
+
+  /* Find pokémon name from Array(allPokemonsWithNames) by number */
+  var pokemonNameByNumber = allPokemonsWithNames.find(function(pokemon) {
+    return pokemon.number == randomPokemon;
+  });
+
+  /* Set pokémon name found to html */
+  pokemonName.textContent = pokemonNameByNumber.name;
+
+  /* 
+    Wait 500ms to show (fadeIn) `.pokemon-found` div,
+    while `.pokeball` is running a (shake) animation.
+  */
+  setTimeout(function() {
+    pokemonFound.style.display = "block";
+    pokemonFound.classList.add("animated", "fadeIn");
+    pokeball.classList.remove("animated", "shake");
+  }, 500);
+});
+
+/*
+  (fadeIn) removed from `.pokemon-found` to add (fadeOut) animation.
+  Wait 500ms to complete (fadeOut) effect.
+*/
+closePokemonFound.addEventListener("click", function() {
+  pokeballAudio.pause();
+  pokeballAudio.currentTime = 0;
+
+  pokemonFound.classList.remove("animated", "fadeIn");
+  pokemonFound.classList.add("animated", "fadeOut");
+
+  setTimeout(function() {
+    pokemonFound.style.display = "none";
+    pokemonFound.classList.remove("animated", "fadeOut");
+  }, 500);
+});
